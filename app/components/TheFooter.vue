d3d825e0da4245992d05b6fe66aafa07a971ca69
whiterqbbit <whiterqbbit@proton.me>
2025-09-10 08:18:10 +0200

    ✨ (footer): add GitHub link

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -8,6 +8,9 @@
       <img src="/chasti_logo_md.png" alt="Mon Journal IEF" class="size-8 rounded-md">
       <span class="font-semibold text-theme-surface-500">Mon Journal IEF</span>
     </div>
+
+    <LinkGithub url="https://github.com/mon-journal-ief" />
+
     <div class="md:ml-auto text-theme-surface-400 text-sm">
       © {{ new Date().getFullYear() }} Mon journal IEF — Tous droits réservés
     </div>
